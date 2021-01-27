86e6db1f05eed9d65abed5cbe20ea0daebfd926c
Cinthya Lins <cml2@cin.ufpe.br>
2021-01-27 16:30:30 -0300

    end of projetinhoReact

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
 import api from '../services/api';
-import { Container, Form, SubmitButton, List } from './styles';
+
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 // import { Container } from './styles';
 
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../services/api';
+import { Loading, Owner, IssueList } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
   state = {
@@ -28,11 +31,44 @@ export default class Repository extends Component {
       issues: issues.data,
       loading: false,
     });
+
+    console.log(repository);
+    console.log(issues);
   }
 
   render() {
     const { repository, issues, loading } = this.state;
 
-    return <h1>Repository</h1>;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map((issue) => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
